f8d2045225fc2cf1f3b825e833d4057cb10f6cd8
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-03-31 16:13:15 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function notfound(){
+    window.location.href = "404NotFound.html";
+}
+
+function youtube(){
+    window.location.href = "https://www.youtube.com/"
+}
+
+function twitter(){
+    window.location.href = "https://www.twitter.com/"
+}
+
+function linkedin() {
+    window.location.href = "https://www.linkedin.com/"
+}
