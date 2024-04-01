724ee47d7f23c8d01db811a0265db5624bbbbd65
Ismoil Tohirov <ismoiltohirov55@gmail.com>
2024-04-01 10:10:08 +0500

    updated

--- main.js
+++ main.js
@@ -3,13 +3,25 @@ function notfound(){
 }
 
 function youtube(){
-    window.location.href = "https://www.youtube.com/"
+    window.location.href = "https://www.youtube.com/";
 }
 
 function twitter(){
-    window.location.href = "https://www.twitter.com/"
+    window.location.href = "https://www.twitter.com/";
 }
 
-function linkedin() {
-    window.location.href = "https://www.linkedin.com/"
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/";
+}
+
+function purchase(){
+    window.location.href = "purchase.html";
+}
+
+function About(){
+    window.location.href = "AboutUs.html";
+}
+
+function Enterprise(){
+    window.location.href = "ForEnterprise.html";
 }
